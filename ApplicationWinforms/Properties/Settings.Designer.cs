0d9918db85e9704f52a3c5ecc35041108038b160
Rihab-Achouri <rihabachouri45@gmail.com>
2020-05-06 02:16:45 +0200

    jhgfdsdr

--- ApplicationWinforms/Properties/Settings.Designer.cs
+++ ApplicationWinforms/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace ApplicationWinforms.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.3.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -30,6 +30,17 @@ namespace ApplicationWinforms.Properties {
         public string cabinetdentaireConnectionString {
             get {
                 return ((string)(this["cabinetdentaireConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\p\\Source\\Repos\\Rihab-Achou" +
+            "ri\\PFA_Reclamation_client\\DAL\\Reclamation_Client.accdb")]
+        public string Reclamation_ClientConnectionString {
+            get {
+                return ((string)(this["Reclamation_ClientConnectionString"]));
             }
         }
     }
